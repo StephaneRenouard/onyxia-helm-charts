c21875ad18a20abf96eb1b913aa8e046ba0804fa
Stephane Renouard <stephane.renouard@gmail.com>
2025-12-25 18:41:49 +0100

    chore(base): scripts build et push

---
+++ base/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+IMAGE_TAG="${IMAGE_TAG:-0.1.0}"
+IMAGE_REPOSITORY="${IMAGE_REPOSITORY:-stephanerenouard/onyxia-code-server}"
+DOCKERFILE="${DOCKERFILE:-code-server.dockerfile}"
+CODE_SERVER_VERSION="${CODE_SERVER_VERSION:-4.106.3}"
+
+docker build \
+  --build-arg "CODE_SERVER_VERSION=${CODE_SERVER_VERSION}" \
+  -t "${IMAGE_REPOSITORY}:${IMAGE_TAG}" \
+  -f "${DOCKERFILE}" \
+  ..
+
---
+++ base/build_and_push.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+IMAGE_TAG="${IMAGE_TAG:-0.1.0}"
+IMAGE_REPOSITORY="${IMAGE_REPOSITORY:-stephanerenouard/onyxia-code-server}"
+DOCKERFILE="${DOCKERFILE:-code-server.dockerfile}"
+CODE_SERVER_VERSION="${CODE_SERVER_VERSION:-4.106.3}"
+
+docker login
+docker build \
+  --build-arg "CODE_SERVER_VERSION=${CODE_SERVER_VERSION}" \
+  -t "${IMAGE_REPOSITORY}:${IMAGE_TAG}" \
+  -f "${DOCKERFILE}" \
+  ..
+docker push "${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+
